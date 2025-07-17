1dddaa92eea31354a2145684f78a0795cfcb106f
Jesus Samano <jesus.martinsamano@gmail.com>
2025-07-17 15:27:03 +0900

    feat: Update initialValues in QuotationForm to remove default values and commented validation schema

--- src/dashboard/quotation/components/QuotationForm/QuotationForm.data.js
+++ src/dashboard/quotation/components/QuotationForm/QuotationForm.data.js
@@ -1,14 +1,8 @@
 import * as Yup from 'yup';
 
 export const initialValues = {
-    registration_number: "RN_1",
-    comercial_name: "CN_1",
-    // legal_representative: "LR_1",
-    // address: "Address 1",
-    // type_of_business: "Type of Business 1",
-    // category: "Category 1",
-    // tel_fax: "TF_1",
-    // website: "https://google.com.mx",
+    registration_number: "",
+    comercial_name: "",
 
     date: "2025-06-04",
     customer: "customer_1",
@@ -34,18 +28,7 @@ export const initialValues = {
     total_price_number: ""
 }
 
-
 export const validationSchema = [
-    // Yup.object({
-    //     registration_number: Yup.string().required('Campo requerido'),
-    //     comercial_name: Yup.string().required('Campo requerido'),
-    //     legal_representative: Yup.string().required('Campo requerido'),
-    //     address: Yup.string().required('Campo requerido'),
-    //     type_of_business: Yup.string().required('Campo requerido'),
-    //     category: Yup.string().required('Campo requerido'),
-    //     tel_fax: Yup.string().required('Campo requerido'),
-    //     website: Yup.string().url('Debe ser una URL válida').required('Campo requerido')
-    // }),
 
     Yup.object().shape({
         date: Yup.date().required('Campo requerido'),
