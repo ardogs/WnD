1b63f07db8230267f086a31076c72c4449cfdaf0
Jesus Samano <jesus.martinsamano@gmail.com>
2025-07-21 15:08:24 +0900

    refactor: Update layout of SupplierFormFields for improved responsiveness and consistency

--- src/dashboard/quotation/components/QuotationForm/Steps/Step_1/SupplierFormFields.jsx
+++ src/dashboard/quotation/components/QuotationForm/Steps/Step_1/SupplierFormFields.jsx
@@ -5,12 +5,12 @@ export const SupplierFormFields = ({supplier}) => {
 
     return (
         <Flex justify="center" align="center" style={{ height: '100%', width: '100%' }} className='no-select'>
-            <Row gutter={[16, 0]}>
+            <Row >
                 <Col xs={24} xxl={24}>
                     <Title level={5}>Número de registro: <Text type="secondary" name="registration_number">{ supplier?.registration_number || "" }</Text></Title>
                 </Col>
 
-                <Col xs={24} xxl={12}>
+                <Col xs={24} xxl={24}>
                     <Title level={5}>Nombre comercial: <Text type="secondary" name="comercial_name">{ supplier?.comercial_name || "" }</Text></Title>
                 </Col>
 
@@ -22,11 +22,11 @@ export const SupplierFormFields = ({supplier}) => {
                     <Title level={5}>Dirección: <Text type="secondary">{ supplier?.address || "" }</Text></Title>
                 </Col>
 
-                <Col xs={24} xxl={12}>
+                <Col xs={24} xxl={24}>
                     <Title level={5}>Rubro: <Text type="secondary">{ supplier?.type_of_business || "" }</Text></Title>
                 </Col>
 
-                <Col xs={24} xxl={12}>
+                <Col xs={24} xxl={24}>
                     <Title level={5}>Categoria: <Text type="secondary">{ supplier?.category || "" }</Text></Title>
                 </Col>
 
